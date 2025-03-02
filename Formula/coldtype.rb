7b0882abcf9e950bef7eb7898150d2458706d8ff
Nate Whetsell <nathan.whetsell@gmail.com>
2025-03-02 04:55:33 -0500

    coldtype: use brewed cffi

--- Formula/coldtype.rb
+++ Formula/coldtype.rb
@@ -16,6 +16,7 @@ class Coldtype < Formula
   end
 
   depends_on "certifi"
+  depends_on "cffi"
   depends_on "ffmpeg"
   depends_on "libsndfile"
   depends_on "numpy"
@@ -51,11 +52,6 @@ class Coldtype < Formula
   resource "brotli" do
     url "https://files.pythonhosted.org/packages/2f/c2/f9e977608bdf958650638c3f1e28f85a1b075f075ebbe77db8555463787b/Brotli-1.1.0.tar.gz"
     sha256 "81de08ac11bcb85841e440c13611c00b67d3bf82698314928d0b676362546724"
-  end
-
-  resource "cffi" do
-    url "https://files.pythonhosted.org/packages/fc/97/c783634659c2920c3fc70419e3af40972dbaf758daa229a7d6ea6135c90d/cffi-1.17.1.tar.gz"
-    sha256 "1c39c6016c32bc48dd54561950ebd6836e1670f2ae46128f67cf49e789c52824"
   end
 
   resource "charset-normalizer" do
